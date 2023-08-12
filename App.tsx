d7efb1b3a88a421e5928b126681548c29f27637e
Andre Beolchi <andregarbini2001@gmail.com>
2023-08-12 00:27:07 -0300

    Notch and StatusBar Issue

--- App.tsx
+++ App.tsx
@@ -1,15 +1,34 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+
+const isAndroid = Platform.OS === "android";
 
 export default function App() {
 	return (
-		<View
-			style={{
-				flex: 1,
-				alignItems: "center",
-				justifyContent: "center"
-			}}>
-			<Text>Our react native blank canvas</Text>
-		</View>
+		<>
+			<SafeAreaView
+				style={{
+					flex: 1,
+					marginTop: StatusBar.currentHeight
+				}}>
+				<View
+					style={{
+						backgroundColor: "green",
+						padding: 16
+					}}>
+					<Text>Our react native blank canvas</Text>
+				</View>
+				<View
+					style={{
+						flex: 1,
+						padding: 16,
+						backgroundColor: "blue"
+					}}>
+					<Text>list</Text>
+				</View>
+			</SafeAreaView>
+			<ExpoStatusBar style="auto" />
+		</>
 	);
 }
 
