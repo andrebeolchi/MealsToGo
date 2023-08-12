bc442e60f12c46fb197fda79924a31978cc528b1
Andre Beolchi <andregarbini2001@gmail.com>
2023-08-12 01:04:18 -0300

    Implementing SearchBar

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import { Searchbar } from "react-native-paper";
 
 export default function App() {
+	const [searchQuery, setSearchQuery] = useState<string>("");
+
+	const onChangeSearch = (query: string) => setSearchQuery(query);
+
 	return (
 		<>
 			<SafeAreaView style={styles.container}>
 				<View style={styles.search}>
-					<Text>Our react native blank canvas</Text>
+					<Searchbar
+						placeholder="Search"
+						onChangeText={onChangeSearch}
+						value={searchQuery}
+					/>
 				</View>
 				<View style={styles.list}>
 					<Text>list</Text>
@@ -24,7 +33,6 @@ const styles = StyleSheet.create({
 		marginTop: StatusBar.currentHeight,
 	},
 	search: {
-		backgroundColor: "green",
 		padding: 16,
 	},
 	list: {
