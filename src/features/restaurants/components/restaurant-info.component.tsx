00aec2b1f6766a6dfe8a0aed53af1eb635769e56
Andre Beolchi <andregarbini2001@gmail.com>
2023-08-12 11:04:29 -0300

    Restaurant Info Setup 2

--- src/features/restaurants/components/restaurant-info.component.tsx
+++ src/features/restaurants/components/restaurant-info.component.tsx
@@ -1,6 +1,30 @@
 import React from "react";
 import { Text } from "react-native-paper";
 
-export const RestaurantInfo = () => {
-	return <Text>Restaurant Info</Text>;
+interface RestaurantInfoProps {
+	restaurant: {
+		name: string;
+		icon: string;
+		photos: string[];
+		address: string;
+		openingHours: string;
+		rating: number;
+		isClosedTemporarily: boolean;
+	};
+}
+
+export const RestaurantInfo = ({ restaurant = {} }: RestaurantInfoProps) => {
+	const {
+		name = "Some Restaurant",
+		icon,
+		photos = [
+			"https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		],
+		address = "100 some random street",
+		isOpenNow = true,
+		rating = 4,
+		isClosedTemporarily,
+	} = restaurant;
+
+	return <Text>{name}</Text>;
 };
