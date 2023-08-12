021b45f9a66ccbc5049c23a6dd77298079618268
Andre Beolchi <andregarbini2001@gmail.com>
2023-08-12 10:59:02 -0300

    Restaurant Info Setup 1

--- App.tsx
+++ App.tsx
@@ -1,43 +1,12 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
-import { Searchbar } from "react-native-paper";
+import React from "react";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurant-screen";
 
 export default function App() {
-	const [searchQuery, setSearchQuery] = useState<string>("");
-
-	const onChangeSearch = (query: string) => setSearchQuery(query);
-
 	return (
 		<>
-			<SafeAreaView style={styles.container}>
-				<View style={styles.search}>
-					<Searchbar
-						placeholder="Search"
-						onChangeText={onChangeSearch}
-						value={searchQuery}
-					/>
-				</View>
-				<View style={styles.list}>
-					<Text>list</Text>
-				</View>
-			</SafeAreaView>
+			<RestaurantsScreen />
 			<ExpoStatusBar style="auto" />
 		</>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		marginTop: StatusBar.currentHeight,
-	},
-	search: {
-		padding: 16,
-	},
-	list: {
-		flex: 1,
-		padding: 16,
-		backgroundColor: "blue",
-	},
-});
---
+++ src/features/restaurants/screens/restaurant-screen.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { StatusBar, StyleSheet, View } from "react-native";
+import { Searchbar } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { RestaurantInfo } from "../components/restaurant-info.component";
+
+export const RestaurantsScreen = () => {
+	const [searchQuery, setSearchQuery] = useState<string>("");
+
+	const onChangeSearch = (query: string) => setSearchQuery(query);
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<View style={styles.search}>
+				<Searchbar
+					placeholder="Search"
+					onChangeText={onChangeSearch}
+					value={searchQuery}
+				/>
+			</View>
+			<View style={styles.list}>
+				<RestaurantInfo />
+			</View>
+		</SafeAreaView>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		marginTop: StatusBar.currentHeight,
+	},
+	search: {
+		padding: 16,
+	},
+	list: {
+		flex: 1,
+		padding: 16,
+		backgroundColor: "blue",
+	},
+});
