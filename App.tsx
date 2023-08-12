5c4de2a6aad3897d57aaef3662ad97bffbce45fa
Andre Beolchi <andregarbini2001@gmail.com>
2023-08-12 11:43:20 -0300

    Setting Up Theme

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaView, StatusBar } from "react-native";
-import styled from "styled-components/native";
+import styled, { ThemeProvider } from "styled-components/native";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurant-screen";
+import { theme } from "./src/infrastructure/theme";
 
 const SafeArea = styled(SafeAreaView)`
 	flex: 1;
@@ -12,10 +13,12 @@ const SafeArea = styled(SafeAreaView)`
 export default function App() {
 	return (
 		<>
-			<SafeArea>
-				<RestaurantsScreen />
-			</SafeArea>
-			<ExpoStatusBar style="auto" />
+			<ThemeProvider theme={theme}>
+				<SafeArea>
+					<RestaurantsScreen />
+				</SafeArea>
+				<ExpoStatusBar style="auto" />
+			</ThemeProvider>
 		</>
 	);
 }
