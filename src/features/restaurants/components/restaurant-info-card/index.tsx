593d136bf65b3a64434524ad376745608a3b0850
Andre Beolchi <andregarbini2001@gmail.com>
2023-08-17 18:37:38 -0300

    Moving spacer component

--- src/features/restaurants/components/restaurant-info-card/index.tsx
+++ src/features/restaurants/components/restaurant-info-card/index.tsx
@@ -4,7 +4,7 @@ import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
 import open from "../../../../../assets/open";
 import star from "../../../../../assets/start";
-import { Spacer } from "../spacer";
+import { Spacer } from "../../../../components/spacer";
 import {
 	Address,
 	Info,
