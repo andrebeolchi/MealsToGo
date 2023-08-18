2ada38245e55786f544015db32efdd1ba8889fd5
Andre Beolchi <andregarbini2001@gmail.com>
2023-08-18 15:12:57 -0300

    Setting Up Geocoding Service

---
+++ src/services/location/location.service.ts
@@ -0,0 +1,26 @@
+//@ts-ignore
+import camelize from "camelize";
+import { IResLocationResponse } from "./location.type";
+import { locations } from "./mock";
+
+export const locationResquest = async (
+	searchTerm: string,
+): Promise<IResLocationResponse> => {
+	return new Promise((resolve, reject) => {
+		const locationMock = locations[searchTerm];
+
+		if (!locationMock) {
+			reject("not found");
+		}
+
+		resolve(locationMock);
+	});
+};
+
+export const locationTransform = (result: IResLocationResponse) => {
+	const formattedResponse = camelize(result);
+	const { geometry } = formattedResponse[0];
+	const { lat, lng } = geometry.location;
+
+	return { lat, lng };
+};
---
+++ src/services/location/location.type.ts
@@ -0,0 +1,23 @@
+export interface ILocation {
+	lat: number;
+	lng: number;
+}
+
+export interface IViewport {
+	northeast: ILocation;
+	southwest: ILocation;
+}
+
+export interface IGeometry {
+	location: ILocation;
+	viewport: IViewport;
+}
+
+export interface ILocationResult {
+	geometry: IGeometry;
+}
+
+export interface IResLocationResponse {
+	results: ILocationResult[];
+	status?: string;
+}
